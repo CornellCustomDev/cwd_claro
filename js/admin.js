d06cee5f78bc7bc46679e72abe4ee1e013c8eb99
Alison on Ubuntu VM <2660348+alisonjo315@users.noreply.github.com>
2022-05-24 14:04:16 -0400

    fix last couple cwd_admin mentions

--- js/admin.js
+++ js/admin.js
@@ -8,7 +8,7 @@
 				CKEDITOR.config.fillEmptyBlocks = false;
 				CKEDITOR.config.basicEntities = false;
 				CKEDITOR.config.removePlugins = 'pastefromword';
-				CKEDITOR.config.skin = 'cwd-moono-lisa,/themes/custom/cwd_admin/css/ckeditor/cwd-moono-lisa/'; // allows CSS customization of the editor skin
+				CKEDITOR.config.skin = 'cwd-moono-lisa,/themes/custom/cwd_claro/css/ckeditor/cwd-moono-lisa/'; // allows CSS customization of the editor skin
 				CKEDITOR.config.pasteFilter = 'h2 h3 h4 h5 h6 ul ol li tr caption thead tbody b strong i em sup sub; p(intro); table(table,bordered,flat,striped,colored,rainbow,compact,sectioned); th[*]; td[*]; div(*); a[!href]'; // reduces pasted clutter (only retains basic headings, paragraphs, lists, links, tables, and divs)
 			}
 		}
